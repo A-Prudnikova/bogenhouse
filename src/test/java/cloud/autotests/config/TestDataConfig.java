d27db52ed0c9a71b6957efaee34c30009359d8a5
A-Prudnikova <a.prudnikova@gmail.com>
2021-10-18 20:31:49 +0300

    remote_driver in config

--- src/test/java/cloud/autotests/config/TestDataConfig.java
+++ src/test/java/cloud/autotests/config/TestDataConfig.java
@@ -5,7 +5,8 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:cloud/autotests/config/testdata.properties"
+        "classpath:cloud/autotests/config/testdata.properties",
+
 })
 public interface TestDataConfig extends Config {
     @Key("web.url")
